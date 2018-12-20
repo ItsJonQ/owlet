2cd557a9ef8e8475a66b7d43e52ab532bfb26bd6
ItsJonQ <itsjonq@gmail.com>
2018-12-20 18:07:24 -0500

    Add Night Owl <3

---
+++ src/themes/night-owl.js
@@ -0,0 +1,16 @@
+const colors = require("../colors/default");
+
+const config = {
+  name: "Owlet (Night Owl)",
+  type: "dark"
+};
+
+const shades = {
+  background: "#011627"
+};
+
+module.exports = {
+  config,
+  shades,
+  colors
+};
