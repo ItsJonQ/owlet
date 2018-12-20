4ba80b2b46a1c17e73181586f8ee31adc6790da4
ItsJonQ <itsjonq@gmail.com>
2018-12-20 13:21:16 -0500

    Add Dracula

---
+++ src/themes/dracula.js
@@ -0,0 +1,16 @@
+const colors = require("../colors/default");
+
+const config = {
+  name: "Owlet (Dracula)",
+  type: "dark"
+};
+
+const shades = {
+  background: "#282a36"
+};
+
+module.exports = {
+  config,
+  shades,
+  colors
+};
