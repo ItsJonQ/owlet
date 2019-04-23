01b53dc47f2e2fd4fbd9b82a0846a9dbdeb06b10
Jon Q <hello@jonquach.com>
2019-04-23 17:19:55 -0400

    Update solarized

--- src/themes/solarized.js
+++ src/themes/solarized.js
@@ -1,14 +1,14 @@
 const config = {
-  name: "Owlet (Solarized)",
-  type: "dark"
-};
+  name: 'Owlet (Solarized)',
+  type: 'dark',
+}
 
 const shades = {
-  background: "#0d2b35",
-  text: "#7388ab"
-};
+  background: '#0d2b35',
+  text: '#85a5b4',
+}
 
 module.exports = {
   config,
-  shades
-};
+  shades,
+}
