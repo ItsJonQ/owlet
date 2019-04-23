6eafeefe7dea53c8fae09d57184d16f10ce87768
Jon Q <hello@jonquach.com>
2019-04-23 17:16:40 -0400

    Add Solarized

---
+++ src/themes/solarized.js
@@ -0,0 +1,14 @@
+const config = {
+  name: "Owlet (Solarized)",
+  type: "dark"
+};
+
+const shades = {
+  background: "#0d2b35",
+  text: "#7388ab"
+};
+
+module.exports = {
+  config,
+  shades
+};
